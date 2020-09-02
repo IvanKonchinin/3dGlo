1830f0e48dd3d731a590cf46e9de0618e441e4b9
Ivan Konchinin <vanish7@mail.ru>
2020-09-02 10:54:26 +0300

    feat: make functions - scroll and animation modals

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+  
   //tIMER
-	function countTimer(deadLine) {
-		let timerHours = document.querySelector('#timer-hours'),
-			  timerMinutes = document.querySelector('#timer-minutes'),
-        timerSeconds = document.querySelector('#timer-seconds');
+	const countTimer = (deadLine) => {
+		const timerHours = document.querySelector('#timer-hours'),
+			    timerMinutes = document.querySelector('#timer-minutes'),
+          timerSeconds = document.querySelector('#timer-seconds');
 
-    function getTimeRemaining(){
+    const getTimeRemaining = () => {
       let dateStop = new Date(deadLine).getTime(),
           dateNow = new Date().getTime(),
           timeRemaining = (dateStop - dateNow) / 1000,
@@ -22,7 +23,7 @@ window.addEventListener('DOMContentLoaded', () => {
           return { timeRemaining, hours, minutes, seconds };
     }
 
-      function updateClock(){
+      const updateClock = () => {
       
       let timer = getTimeRemaining();
         if (timer.timeRemaining > 0) {
@@ -42,6 +43,139 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   
   countTimer('02 september 2020');
+
+  //menu
+
+  const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+          menu = document.querySelector('menu'),
+          closeBtn = document.querySelector('.close-btn'),
+          menuItems = menu.querySelectorAll('ul>li'),
+          btnMouse = document.querySelectorAll('main a');
+    
+    const handlerMenu = (e) => {
+      menu.classList.toggle('active-menu');
+      
+    }  
+    //scrollMenu
+    const scrollMenuItems = (e) => {
+      let target = e.target;
+      target = (e.target.tagName !== 'A') ? e.target.closest('a') : e.target;
+      e.preventDefault();
+      let itemHrefAttr = target.getAttribute('href');
+      let blockSearch = document.querySelector(`${itemHrefAttr}`);
+      blockSearch.scrollIntoView({block: 'center', behavior: 'smooth'});
+    }
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+    menuItems.forEach((elem) => elem.addEventListener('click', scrollMenuItems));
+    btnMouse.forEach((elem) => elem.addEventListener('click', scrollMenuItems));
+  }
+  toggleMenu();
+
+  //modal position default
+  const popupContent = document.querySelector('.popup-content');
+  function setTopOuter() {
+    const popup = document.querySelector('.popup');
+    popup.style.cssText = `
+      display:block;
+      opacity:0;
+      transition:.5s;
+      visibility:hidden;
+    `;
+    popupContent.style.top = '-100%';
+  }
+
+  setTopOuter();
+  
+  //popup animation open
+  let modalStatus = false;
+  const modalShow = () => {
+    modalStatus = true;
+    let start = new Date().getTime(),
+        startTop = popupContent.getBoundingClientRect().top,
+        finalTop = (window.innerHeight - popupContent.offsetHeight) / 2,
+        offset = popupContent.offsetHeight + finalTop;
+
+    let popupDrive = function(){
+      let now = new Date().getTime() - start,
+          currTop = Math.round(startTop + offset * now / 400);
+          currTop = (currTop > finalTop) ? finalTop : currTop;
+          popupContent.style.top = currTop + 'px';
+
+          if(currTop < finalTop){
+            requestAnimationFrame(popupDrive);
+          }
+    }
+    requestAnimationFrame(popupDrive);
+  }
+
+  //popup animation close
+  const modalClose = function(){
+    if (modalStatus) {
+        modalStatus = false;
+    
+    let start = new Date().getTime(),
+        startTop = popupContent.getBoundingClientRect().top,
+        finalTop = -popupContent.offsetHeight,
+        offset = popupContent.offsetHeight + (window.innerHeight - popupContent.offsetHeight) / 2;
+
+    let popupDrive = function () {
+      let now = new Date().getTime() - start,
+          currTop = Math.round(startTop - offset * now / 400);
+          currTop = (currTop < finalTop) ? finalTop : currTop;
+          popupContent.style.top = currTop + 'px';
+      if (currTop > finalTop) {
+        requestAnimationFrame(popupDrive);
+      } 
+    }
+      requestAnimationFrame(popupDrive);
+    }
+  } 
+
+  //popup
+  const togglePopup = () => {
+    
+    const popup = document.querySelector('.popup'),
+          popupBtn = document.querySelectorAll('.popup-btn'),
+          popupClose = document.querySelector('.popup-close');
+    
+    popupBtn.forEach((elem) => {
+      elem.addEventListener('click', () => {
+        popup.style.cssText = `
+          opacity:1;
+          transition:.5s;
+          visibility:visible;
+          display:block;
+        `;
+        (window.innerWidth <= 768) ? popupContent.style.top = '10%' : modalShow();
+        
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      (window.innerWidth <= 768) ? popupContent.style.top = '-100%' : modalClose();
+      popup.style.cssText = `
+      opacity:0;
+      transition:.5s;
+      visibility:hidden;
+      display:block;
+    `;
+      
+    });
+    
+  }
+
+//modal position of resize
+  function setTopOpenOuter() {
+    popupContent.style.top = (window.innerHeight - popupContent.offsetHeight) / 2 + 'px';
+  }
+  window.addEventListener('resize', setTopOpenOuter);
+
+  togglePopup();
 });
 
 
