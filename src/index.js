0bd330aab8b73c9755e1f73c849c2516ad22db94
Ivan Konchinin <vanish7@mail.ru>
2020-09-16 18:07:14 +0300

    fix:upgarde on IE

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
   'use strict';
 
+  import 'nodelist-foreach-polyfill';
+  import "@babel/polyfill";
+  import elementClosest from "element-closest";
+  import "formdata-polyfill";
+  import "es6-promise";
+  import "fetch-polyfill";
+  import "element-matches-polyfill";
+  elementClosest(window);
+
+
   import countTimer from './modules/countTimer';
   import toggleMenu from './modules/toggleMenu';
   import setTopOpenOuter from './modules/setTopOpenOuter';
   import setTopOuter from './modules/setTopOuter';
-  // import modalShow from './modules/modalShow';
-  // import modalClose from './modules/modalClose';
   import togglePopup from './modules/togglePopup';
   import tabs from './modules/tabs';
   import slider from './modules/slider';
   import makeDots from './modules/makeDots';
   import changeImgCommand from './modules/changeImgCommand';
+  import validateCalcInputs from './modules/validateCalcInputs';
   import calc from './modules/calc';
   import sendForm from './modules/sendForm';
-  import validateCalcInputs from './modules/validateCalcInputs';
   
   //tIMER
   countTimer('22 september 2020');
   //menu
   toggleMenu();
   //modal position of resize
-  
   window.addEventListener('resize', setTopOpenOuter);
   //modal position default
-  
   setTopOuter()
   //popup animation open
-  
   //popup animation close 
   //popup
   togglePopup();
@@ -34,7 +39,7 @@
   tabs();
   //slider
   //makeDots
-  makeDots(slider);
+ makeDots(slider);
   //changeImgCommand
   document.getElementById('command').addEventListener('mouseover', changeImgCommand);
   document.getElementById('command').addEventListener('mouseout', changeImgCommand);
